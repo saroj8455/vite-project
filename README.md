760002dd26b51a134140d689ffdd018036fee311
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-12-18 22:24:46 +0530

    update api call button click optmize UI

--- README.md
+++ README.md
@@ -272,3 +272,79 @@ export default function DemoOne() {
   );
 }
 ```
+
+## Api Call in useEffect with button click efficent way
+
+```jsx
+import React, { useEffect, useState } from 'react';
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
+import axios from 'axios';
+import { ProgressSpinner } from 'primereact/progressspinner';
+
+const PRICE_PER_ITEM = 5;
+const DUMMY_API = 'https://dummyjson.com/posts/';
+export default function DemoOne() {
+  // Initial value for postId
+  const [postId, setPostId] = useState(1);
+
+  const handelChange = () => {
+    // Pass a random postId
+    setPostId(Math.floor(Math.random() * 100));
+  };
+
+  return (
+    <div className='px-4 py-8 md:px-6 lg:px-8 flex gap-2 justify-content-center align-items-center'>
+      <div className='surface-card w-6 sm:w-max border-round shadow-2 p-4'>
+        <PostBody Id={postId} />
+        <div className='card flex justify-content-center'>
+          <Button onClick={handelChange} label='Show me a different post' />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function PostBody({ Id }) {
+  // set initial value as null
+  const [post, setPost] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    // Prevent twice api call
+    const source = axios.CancelToken.source();
+    const getPost = async () => {
+      try {
+        const resp = await axios.get(`${DUMMY_API}${Id}`, {
+          cancelToken: source.token,
+        });
+        // console.log(resp.data);
+        setPost(resp.data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getPost();
+    return () => {
+      source.cancel();
+      console.log('Clean up api call');
+    };
+  }, [Id]);
+  return (
+    <>
+      {loading ? (
+        <div className='card flex justify-content-center py-3'>
+          <ProgressSpinner strokeWidth='2' animationDuration='10s' />
+        </div>
+      ) : (
+        <>
+          <p className='text-center text-6xl font-bold'>{post.title}</p>
+          <p className='text-center font-bold'>{post.body}</p>
+        </>
+      )}
+    </>
+  );
+}
+```
--- src/components/DemoOne.jsx
+++ src/components/DemoOne.jsx
@@ -5,24 +5,42 @@ import axios from 'axios';
 import { ProgressSpinner } from 'primereact/progressspinner';
 
 const PRICE_PER_ITEM = 5;
-
+const DUMMY_API = 'https://dummyjson.com/posts/';
 export default function DemoOne() {
-  const dummyApi = 'https://dummyjson.com/posts/';
+  // Initial value for postId
+  const [postId, setPostId] = useState(1);
+
+  const handelChange = () => {
+    // Pass a random postId
+    setPostId(Math.floor(Math.random() * 100));
+  };
+
+  return (
+    <div className='px-4 py-8 md:px-6 lg:px-8 flex gap-2 justify-content-center align-items-center'>
+      <div className='surface-card w-6 sm:w-max border-round shadow-2 p-4'>
+        <PostBody Id={postId} />
+        <div className='card flex justify-content-center'>
+          <Button onClick={handelChange} label='Show me a different post' />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function PostBody({ Id }) {
   // set initial value as null
   const [post, setPost] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     // Prevent twice api call
     const source = axios.CancelToken.source();
     const getPost = async () => {
       try {
-        const resp = await axios.get(
-          `${dummyApi}${Math.floor(Math.random() * 100)}`,
-          {
-            cancelToken: source.token,
-          }
-        );
+        const resp = await axios.get(`${DUMMY_API}${Id}`, {
+          cancelToken: source.token,
+        });
         // console.log(resp.data);
         setPost(resp.data);
         setLoading(false);
@@ -35,28 +53,19 @@ export default function DemoOne() {
       source.cancel();
       console.log('Clean up api call');
     };
-  }, []);
-
-  const handelChange = () => {};
-
+  }, [Id]);
   return (
-    <div className='px-4 py-8 md:px-6 lg:px-8 flex gap-2 justify-content-center align-items-center'>
-      <div className='surface-card w-6 border-round shadow-2 p-4'>
-        {loading ? (
-          <div className='card flex justify-content-center'>
-            <ProgressSpinner />
-          </div>
-        ) : (
-          <>
-            <p className='text-center font-bold'>{post.title}</p>
-            <p className='text-center font-bold'>{post.body}</p>
-          </>
-        )}
-
-        <div className='card flex justify-content-center'>
-          <Button onClick={handelChange} label={`Find Post`} />
+    <>
+      {loading ? (
+        <div className='card flex justify-content-center py-3'>
+          <ProgressSpinner strokeWidth='2' animationDuration='10s' />
         </div>
-      </div>
-    </div>
+      ) : (
+        <>
+          <p className='text-center text-6xl font-bold'>{post.title}</p>
+          <p className='text-center font-bold'>{post.body}</p>
+        </>
+      )}
+    </>
   );
 }
